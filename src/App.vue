556499313e9ecd5f396b2a8186f20df38548b520
Douglas Neves <douvinhas@gmail.com>
2021-02-26 15:36:55 -0300

    Commit on master

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="container">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import "assets/css/style.css"
+</style>
